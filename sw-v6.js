09ffc95b76c289dd07df8fbcc153437dd5df9aaf
MJM03 <mjm030798@gmail.com>
2026-09-07 06:23:52 -0500

    chore: bump PWA cache for wheel match v6

---
+++ sw-v6.js
@@ -0,0 +1,5 @@
+const CACHE='qbr-v6-wheel-match';
+const ASSETS=['./','./index.html','./styles.css','./arena-v5-patch.css','./wheel-match-v6.css','./app-v6.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
