e32c11274ccd49d346d22f73eb8ccc9586ed980e
MJM03 <mjm030798@gmail.com>
2026-09-07 09:17:29 -0500

    Add V20 activation marker

---
+++ v20-activate.js
@@ -0,0 +1,5 @@
+/* V20 activation sanity check */
+(function(){
+  const pill=document.querySelector('.version-pill');if(pill)pill.textContent='V20 QUETZAL MEGA FIX';
+  document.documentElement.dataset.qbrVersion='20';
+})();
